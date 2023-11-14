3fcf34d14a8caad3520b7a100b6bf90b3243a4af
HoangTH2405 <tranhuyhoang1512@gmail.com>
2023-11-14 19:17:28 +0700

    MyShop

---
+++ MyShop/Controllers/CustomersController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Entities;
+
+namespace MyShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly MyShopContext _context;
+
+        public CustomersController(MyShopContext context)
+        {
+            _context = context;
+        }
+
+       
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+          if (_context.Customers == null)
+          {
+              return NotFound();
+          }
+            return await _context.Customers.ToListAsync();
+        }
+
+       
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(string id)
+        {
+          if (_context.Customers == null)
+          {
+              return NotFound();
+          }
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+       
+  
+       
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(string id)
+        {
+            if (_context.Customers == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+     
+    }
+}
